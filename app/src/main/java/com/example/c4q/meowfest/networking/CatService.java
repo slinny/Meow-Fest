786c5bb038fa3a8ab6443ca38f715ab2b0b5d0b0
slinny <siranli411@gmail.com>
2019-03-04 23:27:04 -0500

    added networking package

---
+++ app/src/main/java/com/example/c4q/meowfest/networking/CatService.java
@@ -0,0 +1,13 @@
+package com.example.c4q.meowfest.networking;
+
+import com.example.c4q.meowfest.model.CatImage;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface CatService {
+    @GET("api/cats?page=0")
+    Call<List<CatImage>> getCatImageList();
+}
