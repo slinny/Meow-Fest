d15b3347c58c7f7a5598173b31228f53ab58b6aa
slinny <siranli411@gmail.com>
2019-03-04 23:20:25 -0500

    added model class and layout xmls

---
+++ app/src/main/java/com/example/c4q/meowfest/model/CatImage.java
@@ -0,0 +1,36 @@
+package com.example.c4q.meowfest.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class CatImage {
+
+    @Expose
+    @SerializedName("description")
+    private String description;
+    @Expose
+    @SerializedName("image_url")
+    private String imageUrl;
+    @Expose
+    @SerializedName("timestamp")
+    private String timestamp;
+    @Expose
+    @SerializedName("title")
+    private String title;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
